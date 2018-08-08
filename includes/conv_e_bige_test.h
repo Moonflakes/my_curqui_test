49a4815a3c177b843c586f918583b56625a0c24b
thiery margot <margot.thiery@gmail.com>
2018-08-08 18:10:30 +0200

    add test conv_e

--- includes/conv_e_bige_test.h
+++ includes/conv_e_bige_test.h
@@ -5,5 +5,34 @@
 
 int		e_basic_01(void);
 int		e_basic_02(void);
+int     e_basic_03(void);
+int     e_basic_04(void);
+int     e_basic_05(void);
+int     e_basic_06(void);
+int     e_basic_07(void);
+int     e_basic_08(void);
+int     e_basic_09(void);
+int     e_basic_10(void);
+int     e_basic_11(void);
+int     e_basic_12(void);
+int     e_devil_00(void);
+int     e_longnb_01(void);
+int     e_longnb_02(void);
+int     e_longnb_03(void);
+int     e_padding_01(void);
+int     e_padding_02(void);
+int     e_padding_03(void);
+int     e_padding_04(void);
+int     e_precision_01(void);
+int     e_precision_02(void);
+int     e_precision_03(void);
+int     e_precision_04(void);
+int     e_precision_05(void);
+int     e_all_01(void);
+int     e_all_02(void);
+int     e_all_03(void);
+int     e_all_04(void);
+int     e_all_05(void);
+int     e_all_06(void);
 
 #endif
