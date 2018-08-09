3e4941d3f6bc08f73fdfd377e0a1ca6db22c5104
thiery margot <margot.thiery@gmail.com>
2018-08-09 11:57:15 +0200

    ajout tests conv_e basic_05

--- conv_e_bige/a05_basic_05.c
+++ conv_e_bige/a05_basic_05.c
@@ -7,12 +7,12 @@ int		e_basic_05(void)
 	int		ret;
 	int		save_stdout;
 
-	ft_write_test_name("%e , 4.1");
+	ft_write_test_name("%e %e %e %e %e %e %e %e , 4.1, 4.01, 4.001, 4.0001, 4.00001, 4.000001, 4.0000001, 4.00000001");
 	ft_connect_stdout(pfd, &save_stdout);
-	data.r1 = ft_printf("%e", 4.1);
+	data.r1 = ft_printf("%e %e %e %e %e %e %e %e", 4.1, 4.01, 4.001, 4.0001, 4.00001, 4.000001, 4.0000001, 4.00000001);
 	data.s1 = ft_get_stdout(pfd, &save_stdout);
 	ft_connect_stdout(pfd, &save_stdout);
-	data.r2 = printf("%e", 4.1);
+	data.r2 = printf("%e %e %e %e %e %e %e %e", 4.1, 4.01, 4.001, 4.0001, 4.00001, 4.000001, 4.0000001, 4.00000001);
 	data.s2 = ft_get_stdout(pfd, &save_stdout);
 	ret = 0;
 	if (data.r1 != data.r2)
